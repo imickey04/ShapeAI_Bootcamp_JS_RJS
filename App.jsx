77150d658bb0e30f32e280beec21009407a0dd94
imickey04 <64948640+imickey04@users.noreply.github.com>
2021-06-07 00:42:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function app() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default app;
---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript & React.js Bootcamp</h1>
+      <br />
+      <hr />
+      <p>
+        Hi my name is Mayank Chanabhatti, I'm doing this project at just one day
+        before the submission(LOL - The so called procrastinator). In this
+        particular Bootcamp I've learnt some basics about JavaScript & React.js.
+        I also enjoyed this Bootcamp a lot.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
